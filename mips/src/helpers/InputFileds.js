cd37b6e10e1f400e1309027b23a477295be3d8a0
palaneACR <palane@acr.org>
2023-06-01 10:56:26 +0530

    Added Generic Input Field

---
+++ mips/src/helpers/InputFileds.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+
+
+
+
+export const BasicTextField = (props) => {
+    const [show, setShowPassword] = useState(false);
+    const IconDetails = props.IconDetails === undefined ? "" : {
+        startAdornment: (
+            <InputAdornment position={props.IconDetails.Position}>
+                {props.IconDetails.IconComponent}
+            </InputAdornment>
+        ),
+    };
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    return (
+        <>
+            <div style={{ fontWeight: 'bold' }} > {props.InputLabel !== undefined ? `${props.InputLabel} : `:""}</div>
+            &nbsp;
+            <TextField
+                variant={props.variantType}
+                placeholder={props.PlaceHolder}
+                autoComplete='off'
+                onChange={props.HandleInputChange}
+                label={props.Label}
+                size={props.Size}
+                InputProps={IconDetails}
+                type={props.type}
+                value={props.InputValue}
+            />
+
+        </>
+    );
+}
+
+export const BasicTextArea = (props) => {
+    const IconDetails = props.IconDetails === undefined ? "" : {
+        startAdornment: (
+            <InputAdornment position={props.IconDetails.Position}>
+                {props.IconDetails.IconComponent}
+            </InputAdornment>
+        ),
+    };
+    return (
+        <>
+            <div style={{ fontWeight: 'bold' }} > {props.InputLabel} &nbsp; :&nbsp;</div>
+            <TextField
+                multiline
+                variant={props.variantType}
+                placeholder={props.PlaceHolder}
+                autoComplete='off'
+                onChange={props.HandleInputChange}
+                label={props.Label}
+                size={props.Size}
+                minRows={props.TextArea.MinRows}
+                maxRows={props.TextArea.MaxRows}
+                type={props.type}
+                InputProps={IconDetails}
+                value={props.InputValue}
+            />
+        </>
+    );
+
+}
